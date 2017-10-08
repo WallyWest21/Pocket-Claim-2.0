3b8a3a8ab17e1e5a34c552a9fe0ee2f90e2f9fcf
Al J <Al J>
2017-10-08 13:58:23 -0400

    Add project files.

---
+++ Pocket Claim 2.0/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Pocket_Claim_2._0
+{
+    [Activity(Label = "Pocket_Claim_2._0", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
